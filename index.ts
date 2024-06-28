2d9510d95c755260157a1aa773689349a106c2fc
FaYMan2 <suavarnavats244@gmail.com>
2024-06-28 20:27:56 +0530

    feat : pinging functions  added

--- index.ts
+++ index.ts
@@ -11,7 +11,7 @@ app.use('/api', monitorRoutes);
 
 const PORT: number = parseInt(process.env.PORT as string, 10) || 3000;
 
-const pinger = async (links : string[]) =>{
+const pinger = async (links : string[]) => {
     const stats = await Promise.all(links.map( async (link) =>{
         const start =  performance.now()
         const res = await fetch(link)
@@ -26,6 +26,22 @@ const pinger = async (links : string[]) =>{
     return stats
 }
 
+  const pingingService = async (PORT : number) =>{
+    const res = await fetch(`localhost:${PORT}/api/monitor`,{
+      method : 'GET'
+    })
+    const data = await res.json()
+    const links =  (data.results.map((value : any) =>{
+      return value.url
+    }))
+
+    const stats = await pinger(links)
+    console.log(stats)
+  }
+  
+
+  pingingService(PORT)
+  
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
